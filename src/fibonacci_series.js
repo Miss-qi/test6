f576821f1f64ffb3b08192fc569355e5fa5b2b31
Miss-qi <1370322806@qq.com>
2016-06-10 00:16:33 +0800

    first commit

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,20 @@
 'use strict';
 
 function fibonacci_series(n) {
-
+	var arr = [];
+	arr[0] = 0;
+	if(n===1){
+		arr[1] =1;
+	}else if(n===2){
+		arr[1]=arr[2]=1;
+	}else{
+		for(var i=3;i<=n;i++){
+			arr[i] = arr[i-1] + arr[i-2];
+			arr[i-2] = arr[i-1];
+			arr[i-1] = arr[i];
+		}
+	}
+	return arr;
 }
 
 module.exports = fibonacci_series;
