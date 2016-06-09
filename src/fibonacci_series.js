6889a6e1f7da42ac0de5c5b4b5213429b50cd6ee
Miss-qi <1370322806@qq.com>
2016-06-10 00:57:13 +0800

    Update fibonacci_series.js

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -2,15 +2,17 @@
 
 function fibonacci_series(n) {
 	var arr = [];
-	if(n===1){
+	var a,b,c;
+	if(n===0){
+		arr.push(0);
+	}else if(n===1){
 		arr.push(0,1);
-	}else if(n===2){
-		arr.push(0,1,1);
 	}else{
-		arr[n] = fibonacci_series(n-1) + fibonacci_series(n-2);
+		for(i = 2;i <= n; i++){
+			arr[i] = arr[i-1] + arr[i-2];
+			arr.push(arr[i]);
+		}
 	}
 	return arr;
 }
-
 module.exports = fibonacci_series;
-
